f45df348e790ffde3bd847e7afac43545f93ce35
Rabindra Tamang <rabindratamang147@gmail.com>
2019-12-16 23:02:35 +0545

    Add new pet componenet

--- adopt-me/src/js/App.js
+++ adopt-me/src/js/App.js
@@ -1,8 +1,23 @@
+const Pet= () =>{
+    return React.createElement(
+            "div",
+            {},[
+                React.createElement("h1",{},"Simba"),
+                React.createElement("h2",{},"Dog"),
+                React.createElement("h2",{},"Husky"),
+            ]);
+}
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet),
+        ]
     );
 };
 
