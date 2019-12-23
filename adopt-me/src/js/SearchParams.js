8874a275dda87612ba16ea287986279c33b37dcc
Rabindra Tamang <rabindratamang147@gmail.com>
2019-12-23 22:18:00 +0545

    Drop down fix

--- adopt-me/src/js/SearchParams.js
+++ adopt-me/src/js/SearchParams.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
 import { Breeds } from "@frontendmasters/pet";
-import useDropDown from "./hooks/useDropdown";
+import useDropdown from "./hooks/useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seatle, WA");
   const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropDown("Breed", "", Breeds);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
   return (
     <div className="search-params">
       <form>
@@ -21,7 +21,6 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
-
         <AnimalDropdown />
         <BreedDropdown />
         <button>Submit</button>
--- adopt-me/src/js/hooks/useDropdown.js
+++ adopt-me/src/js/hooks/useDropdown.js
@@ -3,7 +3,28 @@ import React, { useState } from "react";
 const useDropdown = (label, defaultState, options) => {
   const [state, setState] = useState(defaultState);
   const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
-  const Dropdown = () => {
+
+  // const Dropdown = () => (
+  //   <label htmlFor={id}>
+  //     {label}
+  //     <select
+  //       id={id}
+  //       value={state}
+  //       onChange={e => setState(e.target.value)}
+  //       onBlur={e => updateState(e.target.value)}
+  //       disabled={!options.length}
+  //     >
+  //       <option />
+  //       {options.map(item => (
+  //         <option key={item} value={item}>
+  //           {item}
+  //         </option>
+  //       ))}
+  //     </select>
+  //   </label>
+  // );
+
+  const Dropdown = () => (
     <label htmlFor={id}>
       {label}
       <select
@@ -14,15 +35,14 @@ const useDropdown = (label, defaultState, options) => {
         disabled={options.length === 0}
       >
         <option>All</option>
-        {options.map(item => {
+        {options.map(item => (
           <option key={item} value={item}>
             {item}
-          </option>;
-        })}
+          </option>
+        ))}
       </select>
-    </label>;
-  };
-
+    </label>
+  );
   return [state, Dropdown, setState];
 };
 
