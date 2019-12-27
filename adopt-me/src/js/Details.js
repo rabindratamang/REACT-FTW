2b295f91d61f15d0ad6c8508e06440c7751a248f
Rabindra Tamang <rabindratamang147@gmail.com>
2019-12-28 00:31:36 +0545

    Implement carousel in detail component

--- adopt-me/src/js/Details.js
+++ adopt-me/src/js/Details.js
@@ -1,8 +1,10 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
   state = { loading: true };
+
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -26,6 +28,7 @@ class Details extends React.Component {
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal}-${breed}-${location}`}</h2>
